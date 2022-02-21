a5bd038547f163c25d2662926c22254efbf6c505
Will <twitchifygamers@gmail.com>
2022-02-21 17:06:49 +0000

    initial commit with basic cli

---
+++ cheese.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+@options = {}
+
+OptionParser.new do |opts|
+  opts.on('-v', '--verbose', 'Show extra information') do
+    @options[:verbose] = true
+  end
+
+  opts.on('-c', '--color', 'Enable syntax highlighting') do
+    @options[:syntax_highlighting] = true
+  end
+end.parse!
+
+p @options
